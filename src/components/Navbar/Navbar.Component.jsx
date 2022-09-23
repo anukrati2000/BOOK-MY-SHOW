5e5243348133851b2efbf10393a76a79d3341864
anukrati2000 <rathoreanukrati09@gmail.com>
2022-09-23 12:33:22 +0530

    Completed Movie page and payment gateway

--- src/components/Navbar/Navbar.Component.jsx
+++ src/components/Navbar/Navbar.Component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BiChevronDown, BiMenu, BiSearch } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 function NavSm() {
     return <>
@@ -9,6 +10,7 @@ function NavSm() {
                 <span className='text-gray-400 text-xs flex items-center cursor-pointer hover:text-white'>
                     Delhi NCR <BiChevronDown />
                 </span>
+
             </div>
             <div className='w-8 h-8'>
                 <BiSearch className='w-full h-full' />
@@ -38,41 +40,46 @@ function NavMd() {
 }
 
 function NavLg() {
-    return <>
-        <div className='container flex mx-auto px-4 items-center justify-between'>
-            <div className='flex items-center w-1/2 gap-3'>
-
-                <div className='w-10 h-10'>
-                    <img src='https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png'
-                        alt='logo'
-                        className='w-full h-full'
-                    />
-                </div>
-
-                <div className='w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md'>
-                    <BiSearch />
-                    <input
-                        type='search'
-                        className='w-full bg-transparent border-none focus:outline-none'
-                        placeholder='Search from movies, events, plays, sports and activities'
-                    />
-                </div>
-
-                <div className='flex items-center gap-3 '>
-                    <span className='text-gray-200 text-base flex items-center cursor-pointer hover:text-white'>
-                        Delhi NCR <BiChevronDown />
-                    </span>
-                    <button className='bg-red-600 text-white px-2 py-1 text-sm rounded'>
-                        Sign In
-                    </button>
-                    <div className='w-8 h-8 text-white'>
-                        <BiMenu className='w-full h-full ' />
+    return (
+        <>
+            <div className="container flex mx-auto px-4 items-center justify-between">
+                <div className="flex items-center w-1/2 gap-3">
+                    <div className="w-10 h-10">
+                        <img
+                            src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                            alt="logo"
+                            className="w-full h-full"
+                        />
+                    </div>
+                    <div className="w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md">
+                        <BiSearch />
+                        <input
+                            type="search"
+                            className="w-full bg-transparent border-none focus:outline-none"
+                            placeholder="Search for movies, events, plays, sports and activities"
+                        />
                     </div>
                 </div>
-
+                <div className="flex items-center gap-3">
+                    <span className="text-gray-200 text-base flex items-center cursor-pointer hover:text-white">
+                        Delhi NCR <BiChevronDown />
+                    </span>
+                    <Link
+                        to="/plays"
+                        className="text-gray-200 text-base flex items-center cursor-pointer hover:text-white"
+                    >
+                        Plays
+                    </Link>
+                    <button className="bg-red-600 text-white px-2 py-1 text-sm rounded">
+                        Sign In
+                    </button>
+                    <div className="w-8 h-8 text-white">
+                        <BiMenu className="w-full h-full" />
+                    </div>
+                </div>
             </div>
-        </div>
-    </>;
+        </>
+    )
 }
 
 // Main Component
